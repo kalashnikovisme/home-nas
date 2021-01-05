bf37d836046246e9b37061636595abef1af26e30
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-01-05 05:25:18 +0400

    Setting navbar in admin panel

--- config/initializers/tramway.rb
+++ config/initializers/tramway.rb
@@ -1,1 +1,13 @@
 Tramway::Core.initialize_application name: :indahouse_nas
+
+Tramway::Admin.set_available_models(
+  Tramway::User::User,
+  Photo,
+  project: :indahouse_nas
+)
+
+Tramway::Admin.navbar_structure(
+  Photo,
+  Tramway::User::User,
+  project: :indahouse_nas
+)
